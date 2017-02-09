3941f204b6c55863d960fc3167824d3dd2f6bb85
Lionel G <lionelg3@gmail.Com>
2017-02-09 23:51:35 +0100

    Part 7 : OK

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 
 var app = express();
 
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', handlebars({
@@ -28,6 +31,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({ secret: 'GFR"fggds§!', resave: false, saveUninitialized: false }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
